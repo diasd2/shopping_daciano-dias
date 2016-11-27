5e70c7f836344b8afab9f5a02d9458f5a22a4d88
diasd2 <daciano.miguel@gmail.com>
2016-11-27 19:40:58 +0000

    Base setup without styles and unit tests

---
+++ src/shoppingCart.directives.js
@@ -0,0 +1,84 @@
+'use strict';
+
+
+angular.module('shoppingCart.directives', [])
+
+    .controller('CartController',['$scope', 'shoppingCart', function($scope, ngCart) {
+        $scope.ngCart = ngCart;
+    }])
+
+    .directive('shoppingcartAddtocart', ['shoppingCart', function(ngCart){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {
+                id:'@',
+                name:'@',
+                quantity:'@',
+                quantityMax:'@',
+                price:'@',
+                data:'='
+            },
+            transclude: true,
+            templateUrl: function(element, attrs) {
+                if ( typeof attrs.templateUrl == 'undefined' ) {
+                    return 'src/views/addtocart.html';
+                } else {
+                    return attrs.templateUrl;
+                }
+            },
+            link:function(scope, element, attrs){
+                scope.attrs = attrs;
+                scope.inCart = function(){
+                    return  ngCart.getItemById(attrs.id);
+                };
+
+                if (scope.inCart()){
+                    scope.q = ngCart.getItemById(attrs.id).getQuantity();
+                } else {
+                    scope.q = parseInt(scope.quantity);
+                }
+
+                scope.qtyOpt =  [];
+                for (var i = 1; i <= scope.quantityMax; i++) {
+                    scope.qtyOpt.push(i);
+                }
+
+            }
+
+        };
+    }])
+
+    .directive('shoppingcartCart', [function(){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {},
+            templateUrl: function(element, attrs) {
+                if ( typeof attrs.templateUrl == 'undefined' ) {
+                    return 'src/views/cart.html';
+                } else {
+                    return attrs.templateUrl;
+                }
+            },
+            link:function(scope, element, attrs){
+
+            }
+        };
+    }])
+
+    .directive('shoppingcartSummary', [function(){
+        return {
+            restrict : 'E',
+            controller : 'CartController',
+            scope: {},
+            transclude: true,
+            templateUrl: function(element, attrs) {
+                if ( typeof attrs.templateUrl == 'undefined' ) {
+                    return 'src/views/summary.html';
+                } else {
+                    return attrs.templateUrl;
+                }
+            }
+        };
+    }]);
